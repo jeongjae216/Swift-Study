49f289b2ddc9c5552ba6252b47959e788ae8d690
Jeongjae Lee <wjdwo0413@naver.com>
2023-02-15 18:15:18 +0900

    스택 자료 구조/ navigationController 이용하여 로그인 화면 만들기

---
+++  navigation controller/ navigation controller/Contrllers/LoginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  LoginViewController.swift
+//   navigation controller
+//
+//  Created by 정재 on 2023/02/15.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    
+    //뷰가 생성 되었을 때
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //상단 네비게이션바 숨김 처리
+        self.navigationController?.isNavigationBarHidden = true
+    }
+
+
+}
+
