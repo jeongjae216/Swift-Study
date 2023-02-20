6a530191f35100313773071b450e428438e52e87
Jeongjae Lee <wjdwo0413@naver.com>
2023-02-20 18:01:36 +0900

    스토리보드로 탭 바 컨트롤러 이용하여 탭 바 만들기

---
+++ 탭 바 만들기/탭 바 만들기/MainTabBarController.swift
@@ -0,0 +1,19 @@
+//
+//  MainTabBarController.swift
+//  탭 바 만들기
+//
+//  Created by 정재 on 2023/02/20.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
